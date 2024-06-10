c9faeffa66eddaa2563628c3cdd42c1ff5b546f3
David Brochart <david.brochart@gmail.com>
2024-06-10 11:52:09 +0200

    Add Connection async context manager

--- sqlite_anyio/sqlite.py
+++ sqlite_anyio/sqlite.py
@@ -5,15 +5,46 @@ __all__ = ["connect", "Connection", "Cursor"]
 import sqlite3
 from collections.abc import Callable, Sequence
 from functools import partial, wraps
+from logging import Logger, getLogger
+from types import TracebackType
 from typing import Any
 
 from anyio import CapacityLimiter, to_thread
 
 
 class Connection:
-    def __init__(self, _real_connection: sqlite3.Connection) -> None:
+    def __init__(
+        self,
+        _real_connection: sqlite3.Connection,
+        _exception_handler: Callable[[type[BaseException], BaseException, TracebackType, Logger], bool] | None = None,
+        _log: Logger | None = None,
+    ) -> None:
         self._real_connection = _real_connection
+        self._exception_handler = _exception_handler
+        self._log = _log or getLogger(__name__)
         self._limiter = CapacityLimiter(1)
+
+    async def __aenter__(self) -> Connection:
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> bool | None:
+        if exc_val is None:
+            await self.commit()  # type: ignore[call-arg]
+            return None
+
+        assert exc_type is not None
+        assert exc_val is not None
+        assert exc_tb is not None
+        await self.rollback()  # type: ignore[call-arg]
+        exception_handled = False
+        if self._exception_handler is not None:
+            exception_handled = self._exception_handler(exc_type, exc_val, exc_tb, self._log)
+        return exception_handled
 
     @wraps(sqlite3.Connection.close)
     async def close(self):
@@ -81,8 +112,24 @@ class Cursor:
         return await to_thread.run_sync(self._real_cursor.fetchall, limiter=self._limiter)
 
 
-async def connect(database: str) -> Connection:
+async def connect(
+    database: str,
+    uri: bool | None = None,
+    exception_handler: Callable[[type[BaseException], BaseException, TracebackType, Logger], bool] | None = None,
+    log: Logger | None = None,
+) -> Connection:
     real_connection = await to_thread.run_sync(
-        partial(sqlite3.connect, database, check_same_thread=False)
+        partial(sqlite3.connect, database, uri=uri, check_same_thread=False)
     )
-    return Connection(real_connection)
+    return Connection(real_connection, exception_handler, log)
+
+
+def exception_logger(
+    exc_type: type[BaseException] | None,
+    exc_val: BaseException | None,
+    exc_tb: TracebackType | None,
+    log: Logger,
+) -> bool:
+    """An exception handler that logs the exception and discards it."""
+    log.error("SQLite exception", exc_info=exc_val)
+    return True  # the exception was handled
