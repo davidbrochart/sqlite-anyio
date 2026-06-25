338a82df66dfed74160c38d9fa56ed9a26f39c47
David Brochart <david.brochart@gmail.com>
2026-06-25 16:49:58 +0200

    implement cancellable queries with interrupt (#15) (#25)
    * implement cancellable statements with interrupt
    
    * tidy
    
    * accept Connection | Cursor first arg
    
    * Use BaseExceptionGroup
    
    * test suite WIP
    
    * test suite coverage passing
    
    * bump anyio to 3.14 for bugfix
    
    * avoid version-dependent exception checking
    
    * try avoiding timed race for test ordering in CI
    
    Co-authored-by: richardsheridan <richard.sheridan@gmail.com>

--- src/sqlite_anyio/sqlite.py
+++ src/sqlite_anyio/sqlite.py
@@ -4,18 +4,86 @@ __all__ = ["connect", "Connection", "Cursor"]
 
 import sqlite3
 import sys
+import threading
 from collections.abc import Callable, Sequence
 from functools import partial
 from logging import Logger, getLogger
 from types import TracebackType
-from typing import Any
+from typing import Any, TypeVar
 
-from anyio import CapacityLimiter, to_thread
+import anyio
+from anyio import to_thread, from_thread
 
 if sys.version_info >= (3, 11):
-    from typing import Self
-else:
-    from typing_extensions import Self  # pragma: nocover
+    from typing import Self, TypeVarTuple, Unpack
+else:  # pragma: nocover
+    from exceptiongroup import BaseExceptionGroup
+    from typing_extensions import Self, TypeVarTuple, Unpack
+
+T_Retval = TypeVar("T_Retval")
+PosArgsT = TypeVarTuple("PosArgsT")
+
+
+async def _interruptible_dispatch(
+    self: Connection | Cursor,
+    func: Callable[[Unpack[PosArgsT]], T_Retval],
+    *args: Unpack[PosArgsT]
+) -> T_Retval:
+    if isinstance(self, Connection):
+        real_connection = self._real_connection
+    elif isinstance(self, Cursor):
+        real_connection = self._real_cursor.connection
+    else:  # pragma: nocover
+        raise AssertionError("Unknown type:", self)
+
+    ev = anyio.Event()
+    lock = threading.Lock()
+    need_interrupt = False
+
+    async def cancel_detector() -> None:
+        try:
+            await ev.wait()
+        except anyio.get_cancelled_exc_class():
+            # Block progress in the thread while checking this flag.
+            # Our guard_interrupt thread only ever holds the lock briefly,
+            # so there's no risk of blocking the event loop.
+            with lock:
+                # Due to race conditions, the first calls to interrupt may be
+                # ignored. This race is quick so this loop should not cycle much.
+                while need_interrupt:
+                    real_connection.interrupt()
+                    await anyio.lowlevel.cancel_shielded_checkpoint()
+            # we do NOT re-raise the cancellation so that the task group
+            # does not swallow our retval. If a Cancelled is to propagate,
+            # it should come out of to_thread.run_sync
+
+    def guard_interrupt() -> T_Retval:
+        nonlocal need_interrupt
+
+        with lock:
+            from_thread.check_cancelled()
+            need_interrupt = True
+        try:
+            return func(*args)
+        except sqlite3.OperationalError as e:
+            if str(e) == "interrupted":
+                from_thread.check_cancelled()
+            raise
+        finally:
+            need_interrupt = False
+
+    try:
+        async with anyio.create_task_group() as g:
+            g.start_soon(cancel_detector)
+            retval = await to_thread.run_sync(guard_interrupt, limiter=self._limiter)
+            ev.set()
+    except BaseExceptionGroup as eg:
+        if len(eg.exceptions) == 1:
+            if isinstance(eg.exceptions[0], Exception):
+                raise eg.exceptions[0]
+        raise  # pragma: nocover (would be an internal error that should fail other tests)
+
+    return retval
 
 
 class Connection:
@@ -28,7 +96,7 @@ class Connection:
         self._real_connection = _real_connection
         self._exception_handler = _exception_handler
         self._log = _log or getLogger(__name__)
-        self._limiter = CapacityLimiter(1)
+        self._limiter = anyio.CapacityLimiter(1)
 
     async def __aenter__(self) -> Self:
         return self
@@ -53,17 +121,19 @@ class Connection:
         return exception_handled
 
     async def execute(self, sql: str, parameters: Sequence[Any] = (), /) -> Cursor:
-        real_cursor = await to_thread.run_sync(self._real_connection.execute, sql, parameters, limiter=self._limiter)
+        real_cursor = await _interruptible_dispatch(self, self._real_connection.execute, sql, parameters)
         return Cursor(real_cursor, self._limiter, self._exception_handler, self._log)
 
     async def close(self) -> None:
-        await to_thread.run_sync(self._real_connection.close, limiter=self._limiter)
+        with anyio.CancelScope(shield=True):
+            await to_thread.run_sync(self._real_connection.close, limiter=self._limiter)
 
     async def commit(self) -> None:
-        await to_thread.run_sync(self._real_connection.commit, limiter=self._limiter)
+        await _interruptible_dispatch(self, self._real_connection.commit)
 
     async def rollback(self) -> None:
-        await to_thread.run_sync(self._real_connection.rollback, limiter=self._limiter)
+        with anyio.CancelScope(shield=True):
+            await to_thread.run_sync(self._real_connection.rollback, limiter=self._limiter)
 
     async def cursor(self, factory: Callable[[sqlite3.Connection], sqlite3.Cursor] = sqlite3.Cursor) -> Cursor:
         real_cursor = await to_thread.run_sync(self._real_connection.cursor, factory, limiter=self._limiter)
@@ -74,7 +144,7 @@ class Cursor:
     def __init__(
         self,
         real_cursor: sqlite3.Cursor,
-        limiter: CapacityLimiter,
+        limiter: anyio.CapacityLimiter,
         _exception_handler: Callable[[type[BaseException], BaseException, TracebackType, Logger], bool] | None,
         _log: Logger,
     ) -> None:
@@ -117,28 +187,29 @@ class Cursor:
         return exception_handled
 
     async def close(self) -> None:
-        await to_thread.run_sync(self._real_cursor.close, limiter=self._limiter)
+        with anyio.CancelScope(shield=True):
+            await to_thread.run_sync(self._real_cursor.close, limiter=self._limiter)
 
     async def execute(self, sql: str, parameters: Sequence[Any] = (), /) -> Cursor:
-        await to_thread.run_sync(self._real_cursor.execute, sql, parameters, limiter=self._limiter)
+        await _interruptible_dispatch(self, self._real_cursor.execute, sql, parameters)
         return self
 
     async def executemany(self, sql: str, parameters: Sequence[Any], /) -> Cursor:
-        await to_thread.run_sync(self._real_cursor.executemany, sql, parameters, limiter=self._limiter)
+        await _interruptible_dispatch(self, self._real_cursor.executemany, sql, parameters)
         return self
 
     async def executescript(self, sql_script: str, /) -> Cursor:
-        await to_thread.run_sync(self._real_cursor.executescript, sql_script, limiter=self._limiter)
+        await _interruptible_dispatch(self, self._real_cursor.executescript, sql_script)
         return self
 
     async def fetchone(self) -> tuple[Any, ...] | None:
-        return await to_thread.run_sync(self._real_cursor.fetchone, limiter=self._limiter)
+        return await _interruptible_dispatch(self, self._real_cursor.fetchone)
 
     async def fetchmany(self, size: int) -> list[tuple[Any, ...]]:
-        return await to_thread.run_sync(self._real_cursor.fetchmany, size, limiter=self._limiter)
+        return await _interruptible_dispatch(self, self._real_cursor.fetchmany, size)
 
     async def fetchall(self) -> list[tuple[Any, ...]]:
-        return await to_thread.run_sync(self._real_cursor.fetchall, limiter=self._limiter)
+        return await _interruptible_dispatch(self, self._real_cursor.fetchall)
 
 
 async def connect(
