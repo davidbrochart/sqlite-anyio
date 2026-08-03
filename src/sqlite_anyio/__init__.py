44167bbe650f8234898474f43374b2e8b52ed2fb
David Brochart <david.brochart@gmail.com>
2026-08-03 19:06:09 +0200

    Allow enabling/disabling cancellation (#28)
    * Allow enabling/disabling cancellation
    
    * Disable cancellation in the browser

--- src/sqlite_anyio/__init__.py
+++ src/sqlite_anyio/__init__.py
@@ -1,9 +1,15 @@
 import importlib.metadata
+import sys
 
 from .sqlite import Connection as Connection
 from .sqlite import Cursor as Cursor
 from .sqlite import connect as connect
+from .sqlite import disable_cancellation as disable_cancellation
+from .sqlite import enable_cancellation as enable_cancellation
 from .sqlite import exception_logger as exception_logger
 
 
 __version__ = importlib.metadata.version("sqlite_anyio")
+
+if sys.platform == "emscripten":
+    disable_cancellation()  # pragma: nocover
